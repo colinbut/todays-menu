f2600d9b61df48b71ac5238d8ac20eea78442006
Colin But <colinbut@users.noreply.github.com>
2018-05-21 17:31:29 +0100

    custom query method in repository to retrieve by date field

--- src/main/java/com/mycompany/todaysmenu/todaysmenu/repository/MenuRepository.java
+++ src/main/java/com/mycompany/todaysmenu/todaysmenu/repository/MenuRepository.java
@@ -9,6 +9,11 @@ import com.mycompany.todaysmenu.todaysmenu.model.Menu;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Date;
+import java.util.Optional;
+
 @Repository
 public interface MenuRepository extends JpaRepository<Menu, Long> {
+
+    Optional<Menu> findByDate(Date date);
 }
