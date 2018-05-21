8f1458e1e596eb16e517d62681f57ba19f80b7a6
Colin But <colinbut@users.noreply.github.com>
2018-05-21 16:42:20 +0100

    initial project

---
+++ src/main/java/com/mycompany/todaysmenu/todaysmenu/TodaysMenuApplication.java
@@ -0,0 +1,12 @@
+package com.mycompany.todaysmenu.todaysmenu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodaysMenuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TodaysMenuApplication.class, args);
+	}
+}
