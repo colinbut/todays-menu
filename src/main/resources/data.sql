2dd365df5b18d6c911fe9c00430b59c22fc310ef
Colin But <colinbut@users.noreply.github.com>
2018-05-21 20:38:29 +0100

    data.sql to initially populate the data

---
+++ src/main/resources/data.sql
@@ -0,0 +1,14 @@
+--  first menu
+insert into menu (DATE) values ({ts '2012-09-17 18:47:52.69'});
+
+insert into starter (dish_name, menu_id) values ('onion bhaji', 1);
+insert into main_course (dish_name, menu_id) values ('king prawn bhuna', 1);
+insert into dessert (dish_name, menu_id) values ('indian dessert', 1);
+
+-- second menu
+
+insert into menu (DATE) values ({ts '2018-05-21 18:47:52.69'});
+
+insert into starter (dish_name, menu_id) values ('selection of fruits', 2);
+insert into main_course (dish_name, menu_id) values ('stir fry chicken and veg', 2);
+insert into dessert (dish_name, menu_id) values ('selection of fruit yoghurts', 2);
