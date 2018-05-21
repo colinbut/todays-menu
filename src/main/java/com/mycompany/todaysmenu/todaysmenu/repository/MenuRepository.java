e5d4f36d4b77f1a47ecfe01156b337088270c5d0
Colin But <colinbut@users.noreply.github.com>
2018-05-21 17:12:14 +0100

    adding the repository

---
+++ src/main/java/com/mycompany/todaysmenu/todaysmenu/repository/MenuRepository.java
@@ -0,0 +1,14 @@
+/*
+ * |-------------------------------------------------
+ * | Copyright © 2018 Colin But. All rights reserved. 
+ * |-------------------------------------------------
+ */
+package com.mycompany.todaysmenu.todaysmenu.repository;
+
+import com.mycompany.todaysmenu.todaysmenu.model.Menu;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface MenuRepository extends JpaRepository<Menu, Long> {
+}
