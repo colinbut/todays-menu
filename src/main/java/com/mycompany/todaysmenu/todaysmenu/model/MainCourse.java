af719fd90b4326170145c4a310f92d1aec95a796
Colin But <colinbut@users.noreply.github.com>
2018-05-21 17:30:34 +0100

    added missing ids

--- src/main/java/com/mycompany/todaysmenu/todaysmenu/model/MainCourse.java
+++ src/main/java/com/mycompany/todaysmenu/todaysmenu/model/MainCourse.java
@@ -9,6 +9,9 @@ import lombok.Data;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.Table;
@@ -17,6 +20,10 @@ import javax.persistence.Table;
 @Table(name = "main_course")
 @Data
 public class MainCourse {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
 
     @Column(name = "dish_name", length = 50, nullable = false)
     private String dishName;
--- src/main/java/com/mycompany/todaysmenu/todaysmenu/model/Starter.java
+++ src/main/java/com/mycompany/todaysmenu/todaysmenu/model/Starter.java
@@ -9,6 +9,9 @@ import lombok.Data;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.Table;
@@ -17,6 +20,10 @@ import javax.persistence.Table;
 @Table(name = "starter")
 @Data
 public class Starter {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
 
     @Column(name = "dish_name", length = 50, nullable = false)
     private String dishName;
